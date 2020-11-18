18f62dd32883f99d2fc773692ee2717aea35af93
Nathan Rezini <nathanreizni@gmail.com>
2020-11-18 05:33:45 -0300

    Config Prettier again

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-import express, { Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
@@ -10,23 +11,23 @@ import './database';
 const app = express();
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory))
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if(err instanceof AppError) {
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
     });
-  };
+  }
 
   console.error(err);
 
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
-  })
+  });
 });
 
 app.listen(3333, () => {
